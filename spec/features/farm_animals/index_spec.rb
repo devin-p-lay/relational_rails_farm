ec046441dbc11f36f9fbcda9538ac6b96adcde42
Paul Leonard <paulpaulleonard@gmail.com>
2021-12-01 21:20:55 -0500

    create farm_animals#index spec

---
+++ spec/features/farm_animals/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Farm Animals Index Page" do
+  before do
+    @farm1 = Farm.create!(name: "Tegriddy Farms", acreage: 100, family_owned: true)
+    @animal1 = Animal.create!(name: "Pepper Pig", age: 3, rescue: true, farm_id: @farm1.id)
+    @animal2 = Animal.create!(name: "Carly Cow", age: 4, rescue: true, farm_id: @farm1.id)
+    visit "farms/#{@farm1.id}/animals"
+  end
+
+  describe "When I visit the Farm's Animals Index page" do
+    it "I see all of the animals from that farm and their attributes" do
+      within "#animal-<%= #{@animal1.id} %>" do
+        expect(page).to have_content(@animal1.name)
+        expect(page).to have_content(@animal1.age)
+        expect(page).to have_content(@animal1.rescue)
+      end
+
+      within "#animal-<%= #{@animal2.id} %>" do
+        expect(page).to have_content(@animal2.name)
+        expect(page).to have_content(@animal2.age)
+        expect(page).to have_content(@animal2.rescue)
+      end
+    end
+  end
+end
