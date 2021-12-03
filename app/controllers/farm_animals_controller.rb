519f4d625bd861516ffd4a5706ccd33e87947960
Devin Pile <devinpile@gmail.com>
2021-12-03 16:32:38 -0700

    Add initial changes to implement story13

--- app/controllers/farm_animals_controller.rb
+++ app/controllers/farm_animals_controller.rb
@@ -1,6 +1,25 @@
 class FarmAnimalsController < ApplicationController
   def index
     @farm = Farm.find(params[:id])
-
   end
+
+  def new
+    @farm = Farm.find(params[:id])
+  end
+
+  def create
+    @farm = Farm.find(params[:id])
+    farm_animal = @farm.animals.create!(fa_params)
+    if farm_animal.save
+      redirect_to "/farms/#{@farm.id}/animals"
+    else
+      redirect_to "/farms/#{@farm.id}/animals/new"
+    end
+  end
+
+  private
+
+    def fa_params
+      params.permit(:name, :age, :rescue, :farm_id)
+    end
 end
