c4fb50b084eb362e79615aa28596015121157bd2
Paul Leonard <paulpaulleonard@gmail.com>
2021-12-04 17:54:30 -0500

    Write tests for new_spec and add test to index spec for farmerchores

---
+++ spec/features/farmer_chores/new_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Farmer Chores New Page' do
+  before do
+    @farmer1 = Farmer.create!(name: "Randy Marsh", age: 45, full_time: true)
+    visit "/farmers/#{@farmer1.id}/chores/new"
+  end
+
+  describe 'when I visit a farmer chores new page' do
+    it "I fill out the form, click submit, i am redirected to index where I see the new chore" do
+      fill_in "Title", with: "Friday Shed Cleanup"
+      fill_in "Duration", with: "2"
+      fill_in "Daily", with: 'no'
+      click_button "Create New Chore"
+    end
+  end
+end
