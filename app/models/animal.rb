e8afe37758b3047f5e1fa3521e5ad16004011379
Paul Leonard <paulpaulleonard@gmail.com>
2021-12-04 22:12:11 -0500

    Fix merge conflict

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,12 +3,9 @@ class Animal < ApplicationRecord
 
   validates_presence_of :name
   validates_presence_of :age
-<<<<<<< HEAD
-=======
   validates_presence_of :rescue
 
   def self.rescue_filter
     where(rescue: true)
-  end 
->>>>>>> 8c9b90b2cd56112ae90956a8629bc16588e9ebf2
+  end
 end
